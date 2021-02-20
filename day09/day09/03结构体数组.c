fe36114e49c0856f772a2c14310afa40e176d62d
zy <zy970628.com>
2021-02-20 16:33:18 +0800

    2021.02.20

--- day09/day09/03结构体数组.c
+++ day09/day09/03结构体数组.c
@@ -5,9 +5,33 @@
 #include <math.h>
 #include <time.h>
 #include <Windows.h>
-
-int main(void)
+struct str04 {
+	char name[20];
+	int age;
+	char sex;
+};
+int main03(void)
 {
+	struct str04 s[2] = { {"张一",26,'f'},{"库的",56,'m'} };
+	struct str04 g[2];
+	for (int i = 0; i < 2; i++) 
+	{
+		printf("姓名=%s     ", s[i].name);
+		printf("年龄=%d\n", s[i].age);
+		printf("性别=%s\n", s[i].sex=='m'?"男":"女");
+	}
+
+	for (int i = 0; i < 2; i++) 
+	{
+		scanf("%s%d,%c", g[i].name, &g[i].age,&g[i].sex);
+	}
+
+	for (int i = 0; i < 2; i++)
+	{
+		printf("姓名=%s     ", g[i].name);
+		printf("年龄=%d\n", g[i].age);
+		printf("性别=%s\n", g[i].sex == 'm' ? "男" : "女");
+	}
 
 	system("pause");
 	return EXIT_SUCCESS;
---
+++ day09/day09/04案例学生成绩.c
@@ -0,0 +1,96 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <Windows.h>
+struct stu1 {
+	char name[20];
+	float scores[3];
+};
+
+struct student {
+	int age;
+	char name[10];
+	int num;
+};
+
+int main0401(void)
+{
+	struct stu1 s[3];
+
+	for (int i = 0; i < 3; i++) 
+	{
+		printf("请输入学生姓名，成绩1，成绩2，成绩3\n");
+		scanf("%s%f%f%f",s[i].name,&s[i].scores[0], &s[i].scores[1], &s[i].scores[2]);
+	}
+
+	for(int i=0;i<3-1;i++)
+		for (int j = 0; j < 3-1-i; j++)
+		{
+			int sum1 = s[j].scores[0] + s[j].scores[1] + s[j].scores[2];
+			int sum2 = s[j+1].scores[0] + s[j+1].scores[1] + s[j+1].scores[2];
+			if (sum1 > sum2) 
+			{
+				//结构体成员交换
+				/*char tmp[20];
+				strcpy(tmp, s[j].name);
+				strcpy(s[j].name, s[j + 1].name);
+				strcpy(s[j + 1].name, tmp);
+
+				for (int k = 0; k < 3; k++)
+				{
+					int tmp = s[j].scores[k];
+					s[j].scores[k] = s[j + 1].scores[k];
+					s[j + 1].scores[k] = tmp;
+				}*/
+
+				//结构体变量交换
+				struct stu1 temp = s[j];
+				s[j] = s[j + 1];
+				s[j + 1] = temp;
+			}
+		}
+
+	for (int i = 0; i < 3; i++)
+	{
+		printf("姓名:%s\n", s[i].name);
+		printf("学生成绩:%.1f,%.1f,%.1f\n", s[i].scores[0], s[i].scores[1], s[i].scores[2]);
+	}
+	
+	system("pause");
+	return EXIT_SUCCESS;
+}
+
+int main(void) 
+{
+	struct student arr[5];
+	arr[0].age = 15;
+	strcpy(arr[0].name, "nanfeng");
+	arr[0].num = 99;
+
+	(*(arr + 1)).age = 16;
+	strcpy((*(arr + 1)).name, "huangtang");
+	(*(arr + 1)).num = 98;
+
+	(arr + 2)->age = 17;
+	strcpy((arr + 2)->name, "yanghu");
+	(arr + 2)->num = 97;
+
+	struct student* p = arr;
+	(*(p + 3)).age = 19;
+	strcpy((*(p + 3)).name, "kexin");
+	(*(p + 3)).num = 95;
+
+	(p + 4)->age = 20;
+	strcpy((p + 4)->name, "yunxin");
+	(p + 4)->num = 94;
+
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	for (int i = 0; i < n; i++)
+		printf("姓名：%s，年龄：%d，成绩：%d\n", (arr + i)->name, (*(arr + i)).age,arr[i].num);
+
+	return EXIT_SUCCESS;
+}
